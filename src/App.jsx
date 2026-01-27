f1972df8a5719d01c58b721a19f9ec7a6a28c346
Temilorun Olawuni <dev.temilorun@gmail.com>
2026-01-27 15:46:51 +0100

    Services Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
+import Services from "./components/Services";
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
@@ -8,6 +10,8 @@ const App = () => {
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme}/>
       <Hero/>
+      <TrustedBy/>
+      <Services/>
     </div>
   )
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import assets from '../assets/assets'
+import Title from './Title'
+import ServiceCard from './ServiceCard'
+
+const Services = () => {
+    const ServicesData = [
+        {
+            tittle: "Advertising",
+            description: "We turn bold ideas into powerful digital solutions that connect, engage...",
+            icon: assets.ads_icon
+        },
+        {
+            tittle: "Content Marketing",
+            description: "We turn bold ideas into powerful digital solutions that connect, engage...",
+            icon: assets.marketing_icon
+        },
+        {
+            tittle: "Content Writing",
+            description: "We turn bold ideas into powerful digital solutions that connect, engage...",
+            icon: assets.content_icon
+        },
+        {
+            tittle: "Social Media",
+            description: "We turn bold ideas into powerful digital solutions that connect, engage...",
+            icon: assets.social_icon
+        },
+        
+    ]
+  return (
+    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>        
+      <img src={assets.bgImage2} className='absolute -top-110 -left-70 -z-1 dark:hidden' alt="" />
+      <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions that move your business forward. '/>
+      <div className='flex flex-col md:grid grid-cols-2'>
+        {ServicesData.map((service, index)=>(
+            <ServiceCard key={index} service={service} index={index}/>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Services
